6169768144b3a8353782c82f24b04ea412e0b3c4
CekicN <cekicnikola53@gmail.com>
2023-07-14 03:51:59 +0200

    Login/Signup

---
+++ app/src/main/java/com/example/camplife/Models/User.kt
@@ -0,0 +1,12 @@
+package com.example.camplife.Models
+
+public class User {
+    public var username:String = "";
+    public var email:String = "";
+
+    constructor(u:String,e:String)
+    {
+        this.username = u;
+        this.email = e;
+    }
+}
